a1316a4c2ea417aeced460d58e6793c78413c6c7
Laurasinabariba <60373218+Laurasinabariba@users.noreply.github.com>
2020-01-28 11:28:09 +0700

    Tugas 1 PBO
    Kalkulator

---
+++ Laura.java
@@ -0,0 +1,24 @@
+
+package laura;
+
+public class Laura {
+
+    public static void main(String[] args) {
+       Lau1 lora = new Lau1();
+        lora.setAngka1(15);
+        lora.setAngka2(3);
+        
+        System.out.println("Hasil Penjumlahan : " + lora.getPenjumlahan());
+        
+        
+        System.out.println("Hasil Pengurangan : " + lora.getPengurangan());
+        
+        
+        System.out.println("Hasil Perkalian : " + lora.getPerkalian());
+        
+       
+        System.out.println("Hasil Pembagian : " + lora.getPembagian());
+        
+    }
+    
+}
